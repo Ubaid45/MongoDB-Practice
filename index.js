317af4ab6a7556df1ec3da50c3477a5dbeec84d2
Ubaid Rana <ubaidrana45@gmail.com>
2020-04-16 19:51:10 +0200

    Saving the recor in DB

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/playground')
+mongoose.connect('mongodb://localhost/playground')
     .then(() => console.log('Connected to Mongo DB.'))
     .catch((err) => console.log('Connected to Mongo DB.', err));
+
+const courseSchema = new mongoose.Schema({
+    name: String,
+    author: String,
+    tags: [String],
+    date: { type: Date, default: Date.now },
+    isPublished: Boolean
+});
+
+const Course = mongoose.model('Course', courseSchema);
+
+async function createCourse() {
+    const course = new Course({
+        name: 'Express.js Course',
+        author: 'Ubaid',
+        tags: ['express', 'backend'],
+        isPublished: true
+    });
+
+    const result = await course.save();
+    console.log(result);
+}
+
+createCourse();
